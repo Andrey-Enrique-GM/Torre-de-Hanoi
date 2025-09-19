9def2f5d75ca0f918b5cfe47c26536309d546443
Andrey <Andrey@DESKTOP-LNK0A8R>
2025-09-19 16:53:53 -0700

    kasane

---
+++ Users/Propietario/Documents/NetBeansProjects/mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject11;
+
+/**
+ *
+ * @author Andrey
+ */
+public class Mavenproject11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("linganguli");
+    }
+}
